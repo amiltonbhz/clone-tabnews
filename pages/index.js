bc0e17ee93a35dd0667c816d8e2c284b5a6f944d
amiltonbhz <71555277+amiltonbhz@users.noreply.github.com>
2025-02-12 18:55:08 +0000

    Alterada a frase.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>O Senhor é Deus, e acima dele não há!</h1>
+    return <h1>O Senhor é Deus, e acima dele não há! A Ele sejam a glória e o louvor para sempre.</h1>
 }
 
 export default Home;
