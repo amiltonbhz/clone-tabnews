3485d3dd5f1efadfff47f79463ddcedc5857783a
amiltonbhz <71555277+amiltonbhz@users.noreply.github.com>
2025-02-18 23:08:03 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>O Senhor é Deus, e acima dele não há! A Ele sejam a glória e o louvor para sempre.</h1>
+function Home() {
+  return (
+    <h1>
+      O Senhor é Deus, e acima dele não há! A Ele sejam a glória e o louvor para
+      sempre.
+    </h1>
+  );
 }
 
 export default Home;
